cfba2e2afc2de8f285e9a3fe8db27d7434c9b166
DhanoaJasjit <jasjitdhanoa49@gmail.com>
2023-04-22 23:38:19 +0530

    3

---
+++ Sum2Num/Sum2Num/Sum2Num.cpp
@@ -0,0 +1,14 @@
+// Sum2Num.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y, z;
+    x = 2;
+    y = 10;
+    z = x + y;
+    cout << z;
+    return z;
+}
